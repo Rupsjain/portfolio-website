b4685e33f6c78cc828486b374ffad2994591cfeb
Rupali <rupali.jain49.rj@gmail.com>
2023-12-06 15:37:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Hi, I am Rupali Jain.", " I am a Frontend Developer."],
+    typeSpeed:150,
+    backSpeed:100,
+    // backDelay:1000,
+    loop:true
+});
